85e9a7324a8b2cfeab7e7be44e393b71d6e2d972
anamariasosam <anamariasosam@gmail.com>
2024-02-21 20:36:16 -0500

    feat(Speakers): Add new elements and remove old images

--- src/sections/speakers/speakers.tsx
+++ src/sections/speakers/speakers.tsx
@@ -1,20 +1,27 @@
 import './speakers.scss'
-import speakersImage from '../../assets/old/img/speakers-image.png'
+
+const SECTION = {
+  TITLE: 'Speakers',
+  DESCRIPTION: 'Envía tu propuesta de charla antes del',
+  DATE: '27 de Febrero',
+  CFP_BUTTON_TEXT: 'Call For Speakers',
+}
 
 export default function Speakers() {
   return (
-    <section className="speakers" id="speakers">
-      <span className="speakers__decor" />
-      <div>
-        <img src={speakersImage} />
-        <p>¿Tu?</p>
+    <section className="speakers">
+      <div className="speakers__title-container">
+        <h2 className="speakers__title">{SECTION.TITLE}</h2>
+        <div className="speakers__icon"></div>
       </div>
       <div className="speakers__container">
-        <h2>Speakers</h2>
-        <p>
-          El Call For Speakers está abierto, envía tu propuesta de charla antes
-          del 27 de Febrero
+        <p className="speakers__description">
+          {SECTION.DESCRIPTION}{' '}
+          <span className="speakers__cfp-date">{SECTION.DATE}</span>
         </p>
+        <button className="speakers__cfp-button">
+          {SECTION.CFP_BUTTON_TEXT}
+        </button>
       </div>
     </section>
   )
