fd1331ea192b277d9d82708d358f6896b915e172
vrilli <vcastro@armatuvaca.com>
2024-02-23 11:17:33 -0500

    header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,18 @@
 import './header.scss'
-import headerImage from '../../assets/old/img/header-img.svg'
+import logoHero from '../../assets/Hero/logoHero.png'
 
 export default function Header() {
   return (
-    <header className="header__container">
-      <div className="header__text header__section">
+    <section className="header__container">
+      <div className="mi-seccion"></div>
+      <div className="container-img">
+        <img className="logo" src={logoHero} />
+        <h1>#ImpactTheFuture Día Internacional de la Mujer</h1>
+        <h2>Postula tu charla.</h2>
+        <p>Abril 13, 2024 Medellín, Colombia</p>
+      </div>
+
+      {/* <div className="header__text header__section">
         <h1 className="header__title">Impact the Future</h1>
         <h2 className="header__subtitle">Día internacional de la mujer 2024</h2>
         <p className="header__date">Abril 13, 2024</p>
@@ -23,8 +31,8 @@ export default function Header() {
         </div>
       </div>
       <div className="header__image header__section">
-        <img src={headerImage} aria-hidden />
-      </div>
-    </header>
+        
+      </div> */}
+    </section>
   )
 }
