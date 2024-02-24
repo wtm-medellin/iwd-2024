b6ba9f33856c23af0003e4c153d310833c3c96e2
vrilli <vcastro@armatuvaca.com>
2024-02-24 12:11:25 -0500

    feat: :art: En modo escritorio funciona perfecto pero para tablets y movil no me quieren funcionar los query

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,35 +4,17 @@ import logoHero from '../../assets/Hero/logoHero.png'
 export default function Header() {
   return (
     <section className="header__container">
-      <div className="mi-seccion"></div>
-      <div className="container-img">
-        <img className="logo" src={logoHero} />
-        <h1>#ImpactTheFuture Día Internacional de la Mujer</h1>
-        <h2>Postula tu charla.</h2>
-        <p>Abril 13, 2024 Medellín, Colombia</p>
-      </div>
-
-      {/* <div className="header__text header__section">
-        <h1 className="header__title">Impact the Future</h1>
-        <h2 className="header__subtitle">Día internacional de la mujer 2024</h2>
-        <p className="header__date">Abril 13, 2024</p>
-        <p className="header__location">
-          Auditorio fundadores, Universidad EAFIT
-        </p>
-        <div className="header__btn-container">
-          <a
-            href="https://sessionize.com/iwd-medellin-2024/"
-            target="_blank"
-            rel="noreferrer"
-            className="header__btn"
-          >
-            Envía tu propuesta de charla
-          </a>
+      <div className="header_container-img">
+        <div className="header_section">
+          <img className="header_logo" src={logoHero} />
+          <h1>#ImpactTheFuture</h1>
+          <h1>Día Internacional</h1>
+          <h1>de la Mujer</h1>
+          <h2>Postula tu charla.</h2>
+          <p>Abril 13, 2024</p>
+          <p> Medellín, Colombia</p>
         </div>
       </div>
-      <div className="header__image header__section">
-        
-      </div> */}
     </section>
   )
 }
