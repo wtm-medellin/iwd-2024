a311790779d99731417d4d4daee8e979fa6e1a7d
manuCastrillonM <10585946+ManuCastrillonM@users.noreply.github.com>
2024-02-24 14:55:36 +0100

    update communities section

--- src/sections/about/about.tsx
+++ src/sections/about/about.tsx
@@ -4,9 +4,19 @@ export default function About() {
   return (
     <section className="about">
       <div className="about__text-container">
-        <h2 className="about__title">Sobre el <span className="about__evento">Evento</span></h2>
+        <h2 className="about__title">
+          Sobre el <span className="about__evento">Evento</span>
+        </h2>
         <p className="about__text">
-          El Día Internacional de la Mujer, que se celebra anualmente en marzo, es un momento reconocido mundialmente para celebrar los logros de las mujeres y crear conciencia sobre las cuestiones que afectan la igualdad de género. Este Día Internacional de la Mujer, no solo honramos las contribuciones tecnológicas de las mujeres, estamos dando forma activamente a lo que viene a continuación. Únete al tema de este año, <span className="about__slogan">'Impactar el futuro'</span>, con una variedad de eventos que invitan a la reflexión, todos diseñados para inspirarte y desafiarte.
+          El Día Internacional de la Mujer, que se celebra anualmente en marzo,
+          es un momento reconocido mundialmente para celebrar los logros de las
+          mujeres y crear conciencia sobre las cuestiones que afectan la
+          igualdad de género. Este Día Internacional de la Mujer, no solo
+          honramos las contribuciones tecnológicas de las mujeres, estamos dando
+          forma activamente a lo que viene a continuación. Únete al tema de este
+          año, <span className="about__slogan">'Impactar el futuro'</span>, con
+          una variedad de eventos que invitan a la reflexión, todos diseñados
+          para inspirarte y desafiarte.
         </p>
         <a
           href="https://developers.google.com/womentechmakers?hl=es-419"
--- src/sections/communities/communities.tsx
+++ src/sections/communities/communities.tsx
@@ -1,53 +1,32 @@
 import './communities.scss'
-import titleDecoration from '../../assets/old/img/sponsors-decor.svg'
 
 export default function Communities() {
   const communities = [
-    'digital-diaspora',
     'geek-girls',
     'pioneras',
-    'pyladies',
+    'digital-diaspora',
     'tech-community',
+    'pyladies',
+    'gdg',
   ]
 
   return (
     <section className="communities">
-      <div className="communities__title communities__section">
-        <div>
-          <h2>Aliados</h2>
-          <p>
-            Gracias a las empresas y organizaciones que hacen posible este
-            evento.
-          </p>
-        </div>
-        <div className="communities__decor">
-          <img src={titleDecoration} />
-        </div>
-      </div>
-      <div className="communities__section">
-        <h3 className="communities__section-title">Comunidades</h3>
-        <ul className="communities__list">
-          {communities.map((element) => {
-            const image = new URL(
-              `../../assets/communities/${element}.svg`,
-              import.meta.url
-            ).href
+      <h2 className="communities__title">Comunidades</h2>
+      <ul className="communities__list">
+        {communities.map((element) => {
+          const image = new URL(
+            `../../assets/communities/${element}.svg`,
+            import.meta.url
+          ).href
 
-            return (
-              <li className="communities__item">
-                <img src={image} />
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-      <div className="communities__section">
-        <h3 className="communities__section-title">Patrocinadores</h3>
-        <p>
-          ¿Tu empresa está interesada en patrocinar? Escríbenos para ser parte
-          del evento a wtm@pionerasdev.co{' '}
-        </p>
-      </div>
+          return (
+            <li>
+              <img src={image} />
+            </li>
+          )
+        })}
+      </ul>
     </section>
   )
 }
