f874dca189e405ff3cbeef77cf91ee3becc92eac
VRILLI CASTRO <90112835+Vrilli@users.noreply.github.com>
2024-03-05 00:21:56 -0500

    Update team.tsx

--- src/assets/data/team.tsx
+++ src/assets/data/team.tsx
@@ -27,6 +27,318 @@ const team = [
       },
     ],
   },
+  {
+    name: 'Greysy Rodriguez',
+    description:
+      'Ingeniera electrónica, Mg gerencia de la Innovación y el conocimiento, TechSister comunidad Geek Girls LatAm, orgnizadora de PionerasDev, WTM Ambassador. Amante de la danza y la tecnología.',
+    picture: 'Greysy-Rodriguez.PNG',
+    social: [
+      {
+        name: 'instagram',
+        url: 'https://www.instagram.com/greysytry',
+      },
+      {
+        name: 'x',
+        url: 'https://x.com/Greysytry',
+      },
+    ],
+  },
+  {
+    name: 'Carolina Castañeda',
+    description: 'Ingeniera de software, WTM Ambassador y  GDG Medellin Lider',
+    picture: 'Carolina-Castañeda.jpeg',
+    social: [
+      {
+        name: 'instagram',
+        url: 'https://www.instagram.com/carocastanedac/',
+      },
+    ],
+  },
+  {
+    name: 'Vrilli Castro',
+    description:
+      'Ing. Ambiental y Desarrolladora Frontend, me encanta estar en comunidad y poder aportar a cerrar brechas de generos y raciales. Lider en Pionerasdev y Digital diaspora.',
+    picture: 'Vrilli-Castro.jpeg',
+    social: [
+      {
+        name: 'instagram',
+        url: 'https://www.instagram.com/vrilly_castro?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D',
+      },
+      {
+        name: 'x',
+        url: 'https://x.com/vrillycastro1',
+      },
+    ],
+  },
+  {
+    name: 'Dani Gómez',
+    description:
+      'Ingeniera de software, líder del equipo Frontend. Organizadora de PionerasDev Medellín y WTM Ambassador. La loquita de los perros y los idiomas.',
+    picture: 'Dani-Gomez.jpg',
+    social: [
+      {
+        name: 'instagram',
+        url: '',
+      },
+      {
+        name: 'x',
+        url: '',
+      },
+    ],
+  },
+  {
+    name: 'Marian Villa',
+    description:
+      'Fullstack DevDesigner I Community Building, Technical Content and Product Design I Google Developer Expert on Web Tehnologies, Mentor in Google Accelerator and WTM Ambassador.',
+    picture: 'Marian-Villa.jpg',
+    social: [
+      {
+        name: 'instagram',
+        url: '',
+      },
+      {
+        name: 'x',
+        url: '',
+      },
+    ],
+  },
+  {
+    name: 'Girlesa Quintero',
+    description:
+      'Ingeniera Administradora y QA Automation Engineer Apasionada por la tecnología y las comunidades. Directora en Women Who Code Medellín, promoviendo la inclusión de mujeres en el sector.',
+    picture: 'Girlesa-Quintero.jpg',
+    social: [
+      {
+        name: 'instagram',
+        url: 'https://www.instagram.com/girlesaqr/',
+      },
+      {
+        name: 'Linked',
+        url: 'https://www.linkedin.com/in/girlesa-quintero-rodr%C3%ADguez-800a5649/',
+      },
+    ],
+  },
+  {
+    name: 'Veronica Rodriguez',
+    description:
+      'Economista y Senior Product Manager con +7 años de experiencia en productos digitales en finanzas, logística y ciberseguridad. Referente de comunidades negras en tecnología, Embajadora de Women Tech Masker y Co-fundadora de Digital Diaspora. Organizadora en PionerasDev.',
+    picture: 'Veronica-Rodriguez.jpeg',
+    social: [
+      {
+        name: 'instagram',
+        url: '',
+      },
+      {
+        name: 'Linkedin',
+        url: '',
+      },
+    ],
+  },
+  {
+    name: 'Elizabeth Rangel',
+    description:
+      'Desarrolladora con 2+ años en .Net/AngularJs, WTM Ambassador y miembro del core team de PionerasDev. Apasionada por romper brechas de género en tecnología.',
+    picture: 'Elizabeth-Rangel.jpg',
+    social: [
+      {
+        name: 'instagram',
+        url: '',
+      },
+      {
+        name: 'Linkedin',
+        url: '',
+      },
+    ],
+  },
+  {
+    name: 'Brianith Navarro',
+    description:
+      "Ing. Industrial de profesión y diseñadora y programadora autodidacta. Community Builder.'Desarrolladora con 2+ años en .Net/AngularJs, WTM Ambassador y miembro del core team de PionerasDev. Apasionada por romper brechas de género en tecnología.",
+    picture: 'Brianith-Navarro.png',
+    social: [
+      {
+        name: 'instagram',
+        url: 'https://www.instagram.com/briaenrose/',
+      },
+      {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/brianithnavarro/',
+      },
+      {
+        name: 'x',
+        url: 'https://twitter.com/briaenrose',
+      },
+    ],
+  },
+  {
+    name: 'Sara Nieto',
+    description:
+      'Desarrolladora de Software con experiencia en React/Angular y Spring. Miembro del Core Team de PionerasDev.',
+    picture: 'Sara-Nieto.jpg',
+    social: [
+      {
+        name: 'instagram',
+        url: 'https://www.instagram.com/sarantogo/',
+      },
+      {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/saranietog/',
+      },
+      {
+        name: 'x',
+        url: 'https://twitter.com/sarantogo',
+      },
+    ],
+  },
+  {
+    name: 'Jennifer Castañeda',
+    description: '',
+    picture: 'Jennife-Castañeda.jpg',
+    social: [
+      {
+        name: 'instagram',
+        url: '',
+      },
+      {
+        name: 'Linkedin',
+        url: '',
+      },
+      {
+        name: 'x',
+        url: '',
+      },
+    ],
+  },
+  {
+    name: 'Valentina Pinzón',
+    description: '',
+    picture: 'Valen-pinzon.JPG',
+    social: [
+      {
+        name: 'instagram',
+        url: 'https://www.instagram.com/v.dev.valen/?next=%2F',
+      },
+      {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/valentina-p-86b3b913a/',
+      },
+    ],
+  },
+  {
+    name: 'Sandy Atencio',
+    description: '',
+    picture: 'Sandy-Atencio.JPG',
+    social: [
+      {
+        name: 'instagram',
+        url: '',
+      },
+      {
+        name: 'Linkedin',
+        url: '',
+      },
+    ],
+  },
+  {
+    name: 'Manuela Guerra',
+    description:
+      'Engineer en Rockwell Automation, promueve el empoderamiento femenino, diversidad e inclusión. Activista por los derechos de comunidades negras, líder de Digital Diáspora.',
+    picture: 'Manuela-Guerra.JPEG',
+    social: [
+      {
+        name: 'instagram',
+        url: 'https://www.instagram.com/digitaldiaspora.la/',
+      },
+      {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/manuela-guerra-08b624207/',
+      },
+    ],
+  },
+  {
+    name: 'Lili Valencia',
+    description:
+      'Ingeniera Ambiental, analista de datos en AIR-E, estudiante ML y Rust. Soy Lider de la comunidad Google Developers Group Medellín. Me motiva construir soluciones tecnoloógicas que impacten positivamente el planeta.Mi frase favorita "En comunidad se aprende y se construye.',
+    picture: 'Lili-ValGo.jpeg',
+    social: [
+      {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/lilivalgo/',
+      },
+    ],
+  },
+  {
+    name: 'Tatiana Rodriguez',
+    description:
+      'Desarrolladora Frontend con pasión por compartir conocimiento y mejorar vidas! En camino hacia una Maestría en Ingeniería de Software, certificada como Scrum Master. Lider en PionerasDev Bucaramanga, ¡promoviendo la inclusión y la diversidad en la industria! Amante de la tecnología, los gatos y los viajes.',
+    picture: 'Tatiana-Rodriguez.png',
+    social: [
+      {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/jessicatatianarodriguez/',
+      },
+    ],
+  },
+  {
+    name: 'Manu Castrillon',
+    description:
+      'Ingeniera de Software en Uber, Google Developer Expert y GitHub Campus Expert Alumni; Me gusta crear contenido sobre tecnología y eventos tech.',
+    picture: 'Manu-Castrillon.png',
+    social: [
+      {
+        name: 'Linkedin',
+        url: '',
+      },
+    ],
+  },
+  {
+    name: 'Aleja Henao',
+    description:
+      'Experta en tecnología y líder apasionada. Se formó en desarrollo, gestión de proyectos, análisis de datos y marketing digital de manera autodidacta. Founding Team y líder de Growth en Dapta.ai',
+    picture: 'Alejandra-Henao.jpg',
+    social: [
+      {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/alejahenaoe',
+      },
+      {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/alejahenaoe/',
+      },
+    ],
+  },
+  {
+    name: 'Ana Sosa',
+    description:
+      'Co-fundadora de Pioneras Developers y una apasionada Ingeniera de Sistemas con especialización en desarrollo Frontend. Con 5 años de experiencia en el campo, trabajando con tecnologías líderes como ReactJS y Angular.',
+    picture: 'Ana-Sosa.jpeg',
+    social: [
+      {
+        name: 'Instagram',
+        url: '',
+      },
+      {
+        name: 'Linkedin',
+        url: '',
+      },
+    ],
+  },
+  {
+    name: 'Roxana Rodriguez',
+    description: '',
+    picture: 'Roxana-Rodriguez.png',
+    social: [
+      {
+        name: 'x',
+        url: 'https://twitter.com/NotionFlows',
+      },
+      {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/notionflows/',
+      },
+    ],
+  },
 ]
 
 export default team
+
