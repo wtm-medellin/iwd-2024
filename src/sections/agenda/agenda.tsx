b147bc2f111d2f45183e4a984d9a6c8e341b440c
Ana Sosa <anamariasosam@gmail.com>
2024-03-20 18:00:58 -0400

    feat(Agenda): Update agenda style

--- src/sections/agenda/agenda.tsx
+++ src/sections/agenda/agenda.tsx
@@ -4,106 +4,159 @@ import './agenda.scss'
 export default function Agenda() {
   const agendaInfo = [
     {
-      from: '9:00 AM',
-      to: '9:30 AM',
-      topicMain: 'Recepción y Networking en la plaza del Auditorio Fundadores',
-      topicSecondary: '',
-      stage: 'main',
-      color: 'green',
+      block: '09:00',
+      talks: [
+        {
+          topicMain: {
+            from: '9:00',
+            to: '9:30',
+            title:
+              'Recepción y Networking en la plaza del Auditorio Fundadores',
+            color: 'blue',
+            speaker: null,
+          },
+          topicSecondary: null,
+        },
+        {
+          topicMain: {
+            from: '9:30',
+            to: '10:00',
+            title: 'Bienvenidas al IWD 2024',
+            color: 'red',
+          },
+          topicSecondary: null,
+        },
+      ],
     },
     {
-      from: '9:30 AM',
-      to: '10:00 AM',
-      topicMain: 'Bienvenidas al IWD 2024',
-      topicSecondary: '',
-      stage: 'main',
-      color: 'blue',
+      block: '10:00',
+      talks: [
+        {
+          topicMain: {
+            from: '10:10',
+            to: '10:40',
+            speaker: 'Catalina Zapata',
+            title:
+              'La Sostenibilidad Digital: La clave para impactar el futuro y ayudar al planeta',
+            color: 'green',
+          },
+          topicSecondary: null,
+        },
+      ],
     },
     {
-      from: '10:10 AM',
-      to: '10:40 AM',
-      topicMain:
-        'La Sostenibilidad Digital: La clave para impactar el futuro y ayudar al planeta - Catalina Zapata',
-      topicSecondary: '',
-      stage: 'main',
-      color: 'green',
+      block: '11:00',
+      talks: [
+        {
+          topicMain: {
+            from: '10:50',
+            to: '11:20 AM',
+            title: 'Hello World: Rust',
+            speaker: 'Lili Valencia',
+            color: 'blue',
+          },
+          topicSecondary: null,
+        },
+        {
+          topicMain: {
+            from: '11:30',
+            to: '12:00',
+            title: 'El dragón que le tenía miedo a volar',
+            speaker: 'Violeta Betancur',
+            color: 'green',
+          },
+          topicSecondary: null,
+        },
+      ],
     },
     {
-      from: '10:50 AM',
-      to: '11:20 AM',
-      topicMain: 'Hello World: Rust - Lili Valencia',
-      topicSecondary: '',
-      stage: 'main',
-      color: 'blue',
+      block: '12:00',
+      talks: [
+        {
+          topicMain: {
+            from: '12:00',
+            to: '13:00',
+            title: 'Foto grupal, Almuerzo y Networking',
+            color: 'red',
+          },
+          topicSecondary: null,
+        },
+      ],
     },
     {
-      from: '11:30 AM',
-      to: '12:00 M',
-      topicMain: 'El dragón que le tenía miedo a volar - Violeta Betancur',
-      topicSecondary: '',
-      stage: 'main',
-      color: 'green',
+      block: '13:00',
+      talks: [
+        {
+          topicMain: {
+            from: '13:15',
+            to: '13:45',
+            title:
+              'Desafíos éticos en la Inteligencia Artificial y el sesgo de género',
+            color: 'blue',
+            speaker: 'Mariana Rodríguez',
+          },
+          topicSecondary: null,
+        },
+      ],
     },
     {
-      from: '12:00 M',
-      to: '1:00 PM',
-      topicMain: 'Foto grupal, Almuerzo y Networking',
-      topicSecondary: '',
-      stage: 'main',
-      color: 'red',
+      block: '14:00',
+      talks: [
+        {
+          topicMain: {
+            from: '14:00',
+            to: '14:45',
+            title: 'Panel de mujeres: Impactando el futuro',
+            color: 'green',
+          },
+          topicSecondary: null,
+        },
+      ],
     },
     {
-      from: '1:00 PM',
-      to: '1:45 PM',
-      topicMain:
-        'Desafíos éticos en la Inteligencia Artificial y el sesgo de género - Mariana Rodríguez',
-      topicSecondary: '',
-      stage: 'main',
-      color: 'blue',
+      block: '15:00',
+      talks: [
+        {
+          topicMain: {
+            from: '15:00',
+            to: '15:30',
+            title:
+              'Transformando la colaboración y organización digital con IA: Casos de uso sobre la IA en Notion para equipos de trabajo',
+            speaker: 'Roxana Rodríguez',
+            color: 'red',
+          },
+          topicSecondary: {
+            title:
+              'Mentorías para empleabilidad, hoja de ruta profesional y Mock Interviews',
+            from: '15:30',
+            to: '16:45',
+            color: 'blue',
+          },
+        },
+      ],
     },
     {
-      from: '2:00 PM',
-      to: '2:45 PM',
-      topicMain: 'Panel de mujeres: Impactando el futuro',
-      topicSecondary: '',
-      stage: 'main',
-      color: 'green',
-    },
-    {
-      from: '3:00 PM',
-      to: '3:30 PM',
-      topicMain:
-        'Transformando la colaboración y organización digital con IA: Casos de uso sobre la IA en Notion para equipos de trabajo - Roxana Rodríguez',
-      topicSecondary:
-        'Mentorías para empleabilidad, hoja de ruta profesional y Mock Interviews',
-      stage: 'main',
-      color: 'blue',
-    },
-
-    {
-      from: '3:30 PM',
-      to: '4:45 PM',
-      topicMain: '',
-      topicSecondary:
-        'Mentorías para empleabilidad, hoja de ruta profesional y Mock Interviews',
-      stage: 'main',
-      color: 'green',
-    },
-    {
-      from: '5:00 PM',
-      to: '5:30 PM',
-      topicMain: 'Rifas y cierre',
-      topicSecondary: '',
-      stage: 'main',
-      color: 'blue',
-    },
-    {
-      from: '5:30 PM',
-      to: '6:00 PM',
-      topicMain: 'Networking',
-      topicSecondary: '',
-      stage: 'main',
-      color: 'red',
+      block: '17:00',
+      talks: [
+        {
+          topicMain: {
+            from: '17:00',
+            to: '17:30',
+            title: 'Rifas y cierre',
+            color: 'blue',
+          },
+          topicSecondary: null,
+        },
+        {
+          topicMain: {
+            from: '17:30',
+            to: '18:00',
+            title: 'Networking',
+            color: 'green',
+          },
+          topicSecondary: null,
+        },
+      ],
     },
   ]
 
@@ -123,38 +176,62 @@ export default function Agenda() {
         </h2>
 
         <div className="agenda__schedule">
-          <div className="agenda__schedule__row">
-            <div className="agenda__schedule__row__hours agenda__schedule__row__cell  agenda__schedule__row__cell--red"></div>
-            <div className=" agenda__schedule__row__cell agenda__schedule__row__cell--red agenda__schedule__row__topic-main">
+          <div className="agenda__schedule__row__cell-header">
+            <div className="agenda__schedule__row__cell">Hora</div>
+            <div className=" agenda__schedule__row__cell">
               Auditorio Fundadores
             </div>
-            <div className="agenda__schedule__row__cell agenda__schedule__row__cell--red agenda__schedule__row__topic-secondary">
-              Laboratorio Bloque 18, piso 2
+            <div className="agenda__schedule__row__cell">
+              Laboratorio Bloque 18 - Piso 2
             </div>
           </div>
-          {agendaInfo.map((row) => {
+          {agendaInfo.map(({ block, talks }) => {
             return (
-              <div className={`agenda__schedule__row `}>
+              <div key={block} className={`agenda__schedule__row `}>
                 <div
-                  className={`agenda__schedule__row__cell agenda__schedule__row__cell--${row.color} agenda__schedule__row__hours`}
+                  className={`agenda__schedule__row__cell agenda__schedule__row__hours`}
                 >
-                  <span className="agenda__schedule__row__hours-number">
-                    {row.from}
-                  </span>{' '}
-                  <span className="agenda__schedule__row__hours-dash">-</span>{' '}
-                  <span className="agenda__schedule__row__hours-number">
-                    {row.to}
-                  </span>
+                  {block}
                 </div>
-                <div
-                  className={`agenda__schedule__row__cell agenda__schedule__row__cell--${row.color} agenda__schedule__row__topic-main`}
-                >
-                  {row.topicMain}
-                </div>
-                <div
-                  className={`agenda__schedule__row__cell agenda__schedule__row__cell--${row.color} agenda__schedule__row__topic-secondary`}
-                >
-                  {row.topicSecondary}
+                <div>
+                  {talks.map(({ topicMain, topicSecondary }) => {
+                    return (
+                      <div
+                        key={topicMain.title}
+                        className="agenda__schedule__row__cell-group"
+                      >
+                        <div
+                          className={`agenda__schedule__row__cell agenda__schedule__row__cell--${topicMain.color} agenda__schedule__row__topic-main`}
+                        >
+                          <div>
+                            <span className="agenda__schedule__row__hours-number">
+                              {topicMain.from} - {topicMain.to}
+                            </span>
+
+                            {topicMain.title}
+                            {topicMain.speaker && (
+                              <span className="agenda__schedule__row__cell-speaker">
+                                - {topicMain.speaker}
+                              </span>
+                            )}
+                          </div>
+                        </div>
+                        <div
+                          className={`agenda__schedule__row__cell agenda__schedule__row__topic-secondary 
+                          ${topicSecondary?.color ? `agenda__schedule__row__cell--${topicSecondary.color}` : 'agenda__schedule__row__cell--empty'}`}
+                        >
+                          {topicSecondary && (
+                            <>
+                              <span className="agenda__schedule__row__hours-number">
+                                {topicSecondary.from} - {topicSecondary.to}
+                              </span>
+                              {topicSecondary.title}
+                            </>
+                          )}
+                        </div>
+                      </div>
+                    )
+                  })}
                 </div>
               </div>
             )
